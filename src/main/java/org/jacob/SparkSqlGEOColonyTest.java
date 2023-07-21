8809868940d8a8956f1387a81314931d32c51a93
zhangzengmin <xiaomin0322@163.com>
2023-07-21 18:31:02 +0800

    update

--- src/main/java/org/jacob/SparkSqlGEOColonyTest.java
+++ src/main/java/org/jacob/SparkSqlGEOColonyTest.java
@@ -15,25 +15,26 @@ public class SparkSqlGEOColonyTest {
 		// 注册 GeoSparkSQL 函数
 		GeoSparkSQLRegistrator.registerAll(spark);
 		
-		String sql  = "SELECT *\r\n" + 
-				" FROM (  \r\n" + 
-				"   SELECT *  \r\n" + 
-				"   FROM ddp_pro_ods.kiwi_track_order o  \r\n" + 
-				"   WHERE 1=1\r\n" + 
-				" ) AS o  \r\n" + 
-				" LEFT JOIN (   \r\n" + 
-				"   SELECT m.pos_code,  objid \r\n" + 
-				"   FROM ddp_pro_ods.KIWI_MYSITE m  \r\n" + 
-				" ) AS m ON m.pos_code = o.StoreId\r\n" + 
-				" LEFT JOIN (\r\n" + 
-				"   SELECT t.mysite_objid, ST_GeomFromWKT(t.pg) as pg\r\n" + 
-				"   FROM ddp_pro_ods.KIWI_TRADEZONEELEMENT t \r\n" + 
-				"   where t.pg is not null \r\n" + 
-				"   and  t.pg != \"\"\r\n" + 
-				"   ) AS t ON t.mysite_objid = m.objid   \r\n" + 
-				"   WHERE 1=1  \r\n" + 
-				"  and t.pg is not null and o.longitude is not null and o.latitude is not null\r\n" + 
-				"  and st_contains(t.pg,st_point(cast(o.longitude as decimal(15,2)), cast(o.latitude  as decimal(15,2)))) = true limit 200  ";
+		String sql  = "SELECT *" + 
+				" FROM ("+ 
+				" SELECT * " + 
+				" FROM ddp_pro_ods.kiwi_track_order o " + 
+				" WHERE 1=1 " + 
+				" ) AS o" + 
+				" LEFT JOIN (   " + 
+				"   SELECT m.pos_code,  objid " + 
+				"   FROM ddp_pro_ods.KIWI_MYSITE m  " + 
+				" ) AS m ON m.pos_code = o.StoreId" + 
+				" LEFT JOIN (" + 
+				"   SELECT t.mysite_objid, ST_GeomFromWKT(t.pg) as pg" + 
+				"   FROM ddp_pro_ods.KIWI_TRADEZONEELEMENT_2 t " + 
+				"   where t.pg is not null " + 
+				"   and  t.pg1 is not null" + 
+				"   and  ST_IsClosed(ST_GeomFromText(t.pg1))" + 
+				"   ) AS t ON t.mysite_objid = m.objid" + 
+				" WHERE 1=1  " + 
+				"  and t.pg is not null and o.longitude is not null and o.latitude is not null" + 
+				"  and st_contains(t.pg,st_point(cast(o.longitude as decimal(15,2)), cast(o.latitude  as decimal(15,2)))) = true limit 200";
 		
 		// 执行地理空间查询
 		Dataset<Row> result = spark.sql(
